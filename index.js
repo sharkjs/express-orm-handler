6995db0a321ced3d1e7dafeccf446bc94bdca3c1
Nami-Doc <vendethiel@hotmail.fr>
2013-06-27 19:12:03 +0200

    build js file

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 module.exports = function(name, handler){
+  var uppercasedName;
+  uppercasedName = name[0].toUpperCase() + name.slice(1);
   handler == null && (handler = function(req, res){
-    return req.session[name + "-id"];
+    return req.session[name + "Id"];
   });
   return function(req, res, next){
-    req["get-" + name] = function(cb){
+    req["get" + uppercasedName] = function(cb){
       var id;
       if (!(id = handler(req, res))) {
         res.end();
